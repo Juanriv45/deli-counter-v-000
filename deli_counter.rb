43965452a240820e6a2cb6446c8caee2f2d82e67
Juan Rivera <juanriv45@gmail.com>
2018-02-07 18:55:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line
-if array.size === 0 then
-  puts "The line is currently empty"
-end
+  if array.size === 0 then
+    puts "The line is currently empty"
+  end
 end
