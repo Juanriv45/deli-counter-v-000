46fbc2bfc85d5112091e81e454ef5fa8f3eb5cbc
Juan Rivera <juanriv45@gmail.com>
2018-02-07 19:10:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,11 @@ katz_deli=[]
 def line(array)
   if array.size === 0 then
     puts "The line is currently empty."
+  else 
+    status = "The line is currently:" 
+    array.each do |name|
+      status << "#{array[name]-1}"+ " #{name}"
+    end
   end
 end
 
