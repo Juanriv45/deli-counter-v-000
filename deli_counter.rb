04c946aa9da396ac16adf8752bd2229ae4677d03
Juan Rivera <juanriv45@gmail.com>
2018-02-07 19:34:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 
 def take_a_number(array,name)
-array << name
-number = array.index("#{name}") + 1
-puts "Welcome, #{name}. You are number #{number} in line."
+  array << name
+  number = array.index("#{name}") + 1
+  puts "Welcome, #{name}. You are number #{number} in line."
 end
